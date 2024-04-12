b2b99d800cc21c99d62e30c43658bc9e65e0ec88
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-04-12 07:01:53 -0700

    DONE

--- 0x01-challenge/status_server/app.py
+++ 0x01-challenge/status_server/app.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """
-Module for API status route
+Web server script using Flask.
+
+This script creates a Flask web server that registers a blueprint (app_views)
+for handling routes and includes a custom error handler for 404 errors.
 """
-from flask import Flask, jsonify
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def not_found(error):
+    """Custom error handler for 404 errors.
 
-@app.route('/api/v1/status', methods=['GET'])
-def status():
-    """ status route handler """
-    return jsonify({"status": "OK"})
+    Returns a JSON response with an error message.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Run the web server
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Set debug=True for development
